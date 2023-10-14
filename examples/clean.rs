8ba0dcf6e8d15120b205d04b53d38cdcad819c5c
Andrew Yourtchenko <ayourtch@gmail.com>
2023-10-14 22:58:07 +0200

    implement Layer for String

--- examples/clean.rs
+++ examples/clean.rs
@@ -5,10 +5,14 @@ fn main() {
     let layers = ETHER!(src = "00:01:02:03:04:05")
         / IP!(src = "1.1.1.1", dst = "2.2.2.2")
         / UDP!(sport = 1234).dport(22)
-        / UDP!().dport(22);
-    println!("Layers: {:#?}", &layers);
+        / UDP!().dport(22)
+        / "Testing123".to_string();
+
+    println!("Layers ({}): {:#?}", layers.layers.len(), &layers);
 
     let udp = &layers[UDP!()];
     println!("UDP Sport: {}", udp.sport);
-    println!("Encoded bytes: {:02x?}", layers.fill().encode());
+
+    let bytes = layers.fill().encode();
+    println!("Encoded bytes: {:02x?}", &bytes);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -671,6 +671,26 @@ impl Default for Ip {
     }
 }
 
+impl Layer for String {
+    fn embox(self) -> Box<dyn Layer> {
+        Box::new(self)
+    }
+    fn box_clone(&self) -> Box<dyn Layer> {
+        Box::new(self.clone())
+    }
+    fn fill(&self, stack: &LayerStack, my_index: usize, out_stack: &mut LayerStack) {
+        out_stack.layers.push(Box::new(self.clone()))
+    }
+    fn encode(
+        &self,
+        stack: &LayerStack,
+        my_index: usize,
+        encoded_layers: &EncodingVecVec,
+    ) -> Vec<u8> {
+        self.as_bytes().to_owned()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
