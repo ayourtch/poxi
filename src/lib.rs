2f86d26a690be7a971974d8f494afd123a8544aa
Andrew Yourtchenko <ayourtch@gmail.com>
2023-10-16 09:32:10 +0200

    add Dot1Q header to decoder

--- src/lib.rs
+++ src/lib.rs
@@ -879,6 +879,47 @@ pub struct Ip {
     pub options: Vec<IpOption>,
 }
 
+fn encode_dot1q_tci<E: Encoder>(
+    me: &dot1Q,
+    stack: &LayerStack,
+    my_index: usize,
+    encoded_layers: &EncodingVecVec,
+) -> Vec<u8> {
+    let vlan: u16 = me.vlan.value() & 0xfff;
+    let dei: u16 = ((me.id.value() as u16) & 1) << 12;
+    let pcp: u16 = ((me.prio.value() as u16) & 7) << 13;
+    E::encode_u16(vlan | dei | pcp)
+}
+
+fn decode_dot1q_tci<D: Decoder>(buf: &[u8], me: &mut dot1Q) -> Option<(u16, usize)> {
+    use std::convert::TryInto;
+
+    let (tci, delta) = u16::decode::<D>(buf)?;
+    let vlan: u16 = tci & 0xfff;
+    let dei: u8 = ((tci >> 12) & 1).try_into().unwrap();
+    let pcp: u8 = ((tci >> 13) & 7).try_into().unwrap();
+    me.id = Value::Set(dei);
+    me.prio = Value::Set(pcp);
+    Some((vlan, delta))
+}
+
+fn fill_tci_auto(layer: &dyn Layer, stack: &LayerStack, my_index: usize) -> Value<u8> {
+    Value::Auto
+}
+
+#[derive(FromStringHashmap, NetworkProtocol, Clone, Debug, Eq, PartialEq)]
+#[nproto(register(ETHERTYPE_LAYERS, Ethertype = 0x8100))]
+pub struct dot1Q {
+    #[nproto(default = 0, encode = Skip, decode = Skip)]
+    pub prio: Value<u8>,
+    #[nproto(default = 0, encode = Skip, decode = Skip)]
+    pub id: Value<u8>,
+    #[nproto(default = 1, encode = encode_dot1q_tci, decode = decode_dot1q_tci, fill = fill_tci_auto)]
+    pub vlan: Value<u16>,
+    #[nproto(next: ETHERTYPE_LAYERS => Ethertype)]
+    pub etype: Value<u16>,
+}
+
 #[derive(FromStringHashmap, NetworkProtocol, Clone, Debug, Eq, PartialEq)]
 #[nproto(decode_suppress)]
 pub struct raw {
--- tests/test_decode.rs
+++ tests/test_decode.rs
@@ -35,3 +35,25 @@ fn decode_test_raw() {
 
     eprintln!("{:?}", &x);
 }
+
+#[test]
+fn decode_test_dot1q() {
+    use scarust::*;
+
+    let x = Ether!()
+        .decode(b"AAAAAABBBBBB\x81\x00\x02\x21\x08\x00\x4523456789\x111234567890123456789012a")
+        .unwrap();
+    println!("decode result: {:?}", &x);
+    assert_eq!(x.indices_of(Ether!()), vec![0]);
+    assert_eq!(x.indices_of(Dot1Q!()), vec![1]);
+    assert_eq!(x.indices_of(IP!()), vec![2]);
+    assert_eq!(x.indices_of(UDP!()), vec![3]);
+    let eth = &x[Ether!()];
+    assert_eq!(eth.dst, "41:41:41:41:41:41".into());
+    assert_eq!(eth.src, "42:42:42:42:42:42".into());
+    assert_eq!(eth.etype, Value::Set(0x8100));
+    let dot1q = &x[Dot1Q!()];
+    assert_eq!(dot1q.vlan, Value::Set(545));
+
+    eprintln!("{:?}", &x);
+}
