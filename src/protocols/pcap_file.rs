9630a7016d236f7630343ec6481af29967b976db
Andrew Yourtchenko <ayourtch@gmail.com>
2023-10-17 15:47:56 +0200

    fix the pcap encoder/decoder
    allow it to work with either endianness

--- src/protocols/pcap_file.rs
+++ src/protocols/pcap_file.rs
@@ -1,11 +1,42 @@
+use crate::encdec::binary_little_endian::BinaryLittleEndian;
 use crate::*;
 
 /*
  * This is a toy pcap encoder/decoder
  */
 
-fn encode_packets<E: Encoder>(
+fn encode_data<E: Encoder>(
     me: &pcapFile,
+    stack: &LayerStack,
+    my_index: usize,
+    encoded_layers: &EncodingVecVec,
+) -> Vec<u8> {
+    let mut out: Vec<u8> = vec![];
+    /*
+    for p in &me.packets {
+        let po = p.encode(stack, my_index, encoded_layers);
+        out.extend_from_slice(&po);
+    }
+    */
+    out
+}
+
+fn decode_data<D: Decoder>(buf: &[u8], me: &mut pcapFile) -> Option<(pcapFileData, usize)> {
+    let mut vp: Vec<pcapPacket> = vec![];
+    let mut ci = 0;
+
+    let dec = match me.magic_number.value() {
+        0xa1b2c3d4 => PcapFileData!().decode_with_decoder::<BinaryBigEndian>(buf),
+        0xd4c3b2a1 => PcapFileData!().decode_with_decoder::<BinaryLittleEndian>(buf),
+        x => {
+            panic!("Architecture {:02x?} not supported", x);
+        }
+    };
+    dec.map(|(lyr, delta)| (lyr.layers_of(PcapFileData!())[0].clone(), delta))
+}
+
+fn encode_packets<E: Encoder>(
+    me: &pcapFileData,
     stack: &LayerStack,
     my_index: usize,
     encoded_layers: &EncodingVecVec,
@@ -18,11 +49,14 @@ fn encode_packets<E: Encoder>(
     out
 }
 
-fn decode_packets<D: Decoder>(buf: &[u8], me: &mut pcapFile) -> Option<(Vec<pcapPacket>, usize)> {
+fn decode_packets<D: Decoder>(
+    buf: &[u8],
+    me: &mut pcapFileData,
+) -> Option<(Vec<pcapPacket>, usize)> {
     let mut vp: Vec<pcapPacket> = vec![];
     let mut ci = 0;
     while ci < buf.len() {
-        if let Some((stk, delta)) = PcapPacket!().decode(buf) {
+        if let Some((stk, delta)) = PcapPacket!().decode_with_decoder::<D>(buf) {
             let mut pkts = stk
                 .layers_of(PcapPacket!())
                 .into_iter()
@@ -38,22 +72,27 @@ fn decode_packets<D: Decoder>(buf: &[u8], me: &mut pcapFile) -> Option<(Vec<pcap
 }
 
 #[derive(NetworkProtocol, Clone, Debug, Eq, PartialEq)]
-#[nproto(encoder(PcapEncoder))]
 pub struct pcapFile {
     pub magic_number: Value<u32>, // magic number  0xa1b2c3d4: no swap required, 0xd4c3b2a1: swapped
+    #[nproto(encode = encode_data, decode = decode_data)]
+    d: pcapFileData,
+}
+
+#[derive(NetworkProtocol, Clone, Debug, Eq, PartialEq)]
+pub struct pcapFileData {
     pub version_major: Value<u16>, // major version number
     pub version_minor: Value<u16>, // minor version number
-    pub thiszone: Value<i32>,     // GMT to local correction
-    pub sigfigs: Value<u32>,      // accuracy of timestamps
-    pub snaplen: Value<u32>,      // max length of captured packets, in octets
-    pub network: Value<u32>,      // data link type
+    pub thiszone: Value<i32>,      // GMT to local correction
+    pub sigfigs: Value<u32>,       // accuracy of timestamps
+    pub snaplen: Value<u32>,       // max length of captured packets, in octets
+    pub network: Value<u32>,       // data link type
     #[nproto(encode = encode_packets, decode = decode_packets)]
     pub packets: Vec<pcapPacket>, // encoded packets
 }
 
 fn decode_packet_data<D: Decoder>(buf: &[u8], me: &mut pcapPacket) -> Option<(Vec<u8>, usize)> {
     println!("Packet in progress: {:?}", &me);
-    let plen = u32::from_be(me.incl_len.value()) as usize;
+    let plen = me.incl_len.value() as usize;
     println!("Included len: {}", plen);
     println!("Buf len: {}", buf.len());
 
