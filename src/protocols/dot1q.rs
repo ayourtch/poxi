4c1b62727d1728985b926aa01f4b94cbff4915d1
Andrew Yourtchenko <ayourtch@gmail.com>
2023-10-19 23:06:48 +0200

    dot1q: rearrange the file

--- src/protocols/dot1q.rs
+++ src/protocols/dot1q.rs
@@ -1,5 +1,18 @@
 use crate::*;
 
+#[derive(FromStringHashmap, NetworkProtocol, Clone, Debug, Eq, PartialEq)]
+#[nproto(register(ETHERTYPE_LAYERS, Ethertype = 0x8100))]
+pub struct dot1Q {
+    #[nproto(default = 0, encode = Skip, decode = Skip)]
+    pub prio: Value<u8>,
+    #[nproto(default = 0, encode = Skip, decode = Skip)]
+    pub id: Value<u8>,
+    #[nproto(default = 1, encode = encode_dot1q_tci, decode = decode_dot1q_tci, fill = fill_tci_auto)]
+    pub vlan: Value<u16>,
+    #[nproto(next: ETHERTYPE_LAYERS => Ethertype)]
+    pub etype: Value<u16>,
+}
+
 fn encode_dot1q_tci<E: Encoder>(
     me: &dot1Q,
     stack: &LayerStack,
@@ -26,17 +39,4 @@ fn decode_dot1q_tci<D: Decoder>(buf: &[u8], me: &mut dot1Q) -> Option<(u16, usiz
 
 fn fill_tci_auto(layer: &dyn Layer, stack: &LayerStack, my_index: usize) -> Value<u8> {
     Value::Auto
-}
-
-#[derive(FromStringHashmap, NetworkProtocol, Clone, Debug, Eq, PartialEq)]
-#[nproto(register(ETHERTYPE_LAYERS, Ethertype = 0x8100))]
-pub struct dot1Q {
-    #[nproto(default = 0, encode = Skip, decode = Skip)]
-    pub prio: Value<u8>,
-    #[nproto(default = 0, encode = Skip, decode = Skip)]
-    pub id: Value<u8>,
-    #[nproto(default = 1, encode = encode_dot1q_tci, decode = decode_dot1q_tci, fill = fill_tci_auto)]
-    pub vlan: Value<u16>,
-    #[nproto(next: ETHERTYPE_LAYERS => Ethertype)]
-    pub etype: Value<u16>,
 }
